9de5b49235c1753b3660f1e35d09f90d3ea861cc
DannyChan0204 <danny.chan@students.plymouth.ac.uk>
2023-11-22 14:35:52 +0800

    Dashboard to booking screen navigation based on button chosen RestaurantBookingAppv3

--- app/src/main/java/com/example/restaurantbookingapp/Booking.java
+++ app/src/main/java/com/example/restaurantbookingapp/Booking.java
@@ -1,8 +1,7 @@
-package com.example.restaurantbookingapp;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+package com.example.restaurantbookingapp;// BookingActivity.java
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Booking extends AppCompatActivity {
 
@@ -10,5 +9,32 @@ public class Booking extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+
+        // Get the buttonId from the intent
+        int buttonId = getIntent().getIntExtra("buttonId", 0);
+
+        // Determine the image resource based on the buttonId
+        int imageResource = 0;
+
+        if (buttonId == R.id.bkbutton) {
+            imageResource = R.drawable.breakfastbookingcolumn;
+        } else if (buttonId == R.id.lunchbutton) {
+            imageResource = R.drawable.lunchbookingcolumn;
+        } else if (buttonId == R.id.dinnerbutton) {
+            imageResource = R.drawable.dinnerbookingcolumn;
+        }
+
+        // Send the chosen image resource back to the Dashboard activity
+        setResultAndFinish(imageResource);
+    }
+
+    private void setResultAndFinish(int imageResource) {
+        // Create an intent to send the result back to the Dashboard activity
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("imageResource", imageResource);
+        setResult(RESULT_OK, resultIntent);
+
+        // Finish the current activity
+        finish();
     }
 }
--- app/src/main/java/com/example/restaurantbookingapp/dashboard.java
+++ app/src/main/java/com/example/restaurantbookingapp/dashboard.java
@@ -1,14 +1,57 @@
-package com.example.restaurantbookingapp;
-
+package com.example.restaurantbookingapp;// DashboardActivity.java
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.restaurantbookingapp.R;
 
 public class dashboard extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_BOOKING_ACTIVITY = 1;
+    private ImageView selectedImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        //selectedImageView = findViewById(R.id.selectedImageView);
+
+        ImageButton bkButton = findViewById(R.id.bkbutton);
+        ImageButton lunchButton = findViewById(R.id.lunchbutton);
+        ImageButton dinnerButton = findViewById(R.id.dinnerbutton);
+
+        View.OnClickListener buttonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Launch the Booking activity
+                Intent intent = new Intent(dashboard.this, Booking.class);
+                intent.putExtra("buttonId", view.getId());
+                startActivityForResult(intent, REQUEST_CODE_BOOKING_ACTIVITY);
+            }
+        };
+
+        bkButton.setOnClickListener(buttonClickListener);
+        lunchButton.setOnClickListener(buttonClickListener);
+        dinnerButton.setOnClickListener(buttonClickListener);
+    }
+
+    // This method will be called when BookingActivity finishes
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Optional: If you don't need any information from BookingActivity, you can skip this check
+        if (requestCode == REQUEST_CODE_BOOKING_ACTIVITY && resultCode == RESULT_OK && data != null) {
+            // Retrieve the chosen image resource from the result (not needed in this case)
+            // int imageResource = data.getIntExtra("imageResource", 0);
+
+            // No need to update the image in the Dashboard activity
+            // selectedImageView.setImageResource(imageResource);
+        }
     }
 }
