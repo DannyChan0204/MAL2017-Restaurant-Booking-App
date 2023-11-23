41c19a923c8a2e82585c23bbba26da906e970642
DannyChan0204 <danny.chan@students.plymouth.ac.uk>
2023-11-23 11:57:47 +0800

    Navigation from dashboard to booking(Solved bug) RestaurantBookingApp v3

--- app/src/main/java/com/example/restaurantbookingapp/Booking.java
+++ app/src/main/java/com/example/restaurantbookingapp/Booking.java
@@ -1,40 +1,26 @@
 package com.example.restaurantbookingapp;// BookingActivity.java
-import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Booking extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
 
-        // Get the buttonId from the intent
-        int buttonId = getIntent().getIntExtra("buttonId", 0);
+        TextView selectedText = findViewById(R.id.breakfasttext);
+        ImageView selectedImage = findViewById(R.id.bookingimage1);
 
-        // Determine the image resource based on the buttonId
-        int imageResource = 0;
+        // Get the data from the intent
+        String meal = getIntent().getStringExtra("meal");
+        int imageResource = getIntent().getIntExtra("imageResource", 0);
 
-        if (buttonId == R.id.bkbutton) {
-            imageResource = R.drawable.breakfastbookingcolumn;
-        } else if (buttonId == R.id.lunchbutton) {
-            imageResource = R.drawable.lunchbookingcolumn;
-        } else if (buttonId == R.id.dinnerbutton) {
-            imageResource = R.drawable.dinnerbookingcolumn;
-        }
-
-        // Send the chosen image resource back to the Dashboard activity
-        setResultAndFinish(imageResource);
-    }
-
-    private void setResultAndFinish(int imageResource) {
-        // Create an intent to send the result back to the Dashboard activity
-        Intent resultIntent = new Intent();
-        resultIntent.putExtra("imageResource", imageResource);
-        setResult(RESULT_OK, resultIntent);
-
-        // Finish the current activity
-        finish();
+        // Update the TextView and ImageView
+        selectedText.setText(meal);
+        selectedImage.setImageResource(imageResource);
     }
 }
--- app/src/main/java/com/example/restaurantbookingapp/dashboard.java
+++ app/src/main/java/com/example/restaurantbookingapp/dashboard.java
@@ -3,23 +3,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.restaurantbookingapp.R;
-
 public class dashboard extends AppCompatActivity {
-
-    private static final int REQUEST_CODE_BOOKING_ACTIVITY = 1;
-    private ImageView selectedImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
-        //selectedImageView = findViewById(R.id.selectedImageView);
 
         ImageButton bkButton = findViewById(R.id.bkbutton);
         ImageButton lunchButton = findViewById(R.id.lunchbutton);
@@ -28,30 +19,27 @@ public class dashboard extends AppCompatActivity {
         View.OnClickListener buttonClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Launch the Booking activity
+                // Launch the Booking activity with the corresponding text and image
                 Intent intent = new Intent(dashboard.this, Booking.class);
-                intent.putExtra("buttonId", view.getId());
-                startActivityForResult(intent, REQUEST_CODE_BOOKING_ACTIVITY);
+
+                int buttonId = view.getId();
+                if (buttonId == R.id.bkbutton) {
+                    intent.putExtra("meal", "Breakfast");
+                    intent.putExtra("imageResource", R.drawable.breakfastbookingcolumn);
+                } else if (buttonId == R.id.lunchbutton) {
+                    intent.putExtra("meal", "Lunch");
+                    intent.putExtra("imageResource", R.drawable.lunchbookingcolumn);
+                } else if (buttonId == R.id.dinnerbutton) {
+                    intent.putExtra("meal", "Dinner");
+                    intent.putExtra("imageResource", R.drawable.dinnerbookingcolumn);
+                }
+
+                startActivity(intent);
             }
         };
 
         bkButton.setOnClickListener(buttonClickListener);
         lunchButton.setOnClickListener(buttonClickListener);
         dinnerButton.setOnClickListener(buttonClickListener);
-    }
-
-    // This method will be called when BookingActivity finishes
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Optional: If you don't need any information from BookingActivity, you can skip this check
-        if (requestCode == REQUEST_CODE_BOOKING_ACTIVITY && resultCode == RESULT_OK && data != null) {
-            // Retrieve the chosen image resource from the result (not needed in this case)
-            // int imageResource = data.getIntExtra("imageResource", 0);
-
-            // No need to update the image in the Dashboard activity
-            // selectedImageView.setImageResource(imageResource);
-        }
     }
 }
